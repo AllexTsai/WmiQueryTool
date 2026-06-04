a68276d051d8241d83ab9108d5e5928ce69cb025
AllexTsai <allex0901@yahoo.com.tw>
2026-06-04 17:59:18 +0800

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Management;
+using System.Windows;
+
+namespace WmiQueryTool
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RunQuery_Click(object sender, RoutedEventArgs e)
+        {
+            ResultBox.Items.Clear();
+            try
+            {
+                string query = QueryBox.Text;
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    foreach (var prop in obj.Properties)
+                    {
+                        ResultBox.Items.Add($"{prop.Name}: {prop.Value}");
+                    }
+                    ResultBox.Items.Add("----");
+                }
+            }
+            catch (Exception ex)
+            {
+                ResultBox.Items.Add($"Error: {ex.Message}");
+            }
+        }
+    }
+}
